078e7f7e61151ab09fa0dd4ce518e784cf713235
wonyong <zcx5674@gmail.com>
2018-05-07 10:06:46 +0900

    conditional probability

--- README.md
+++ README.md
@@ -8,16 +8,6 @@
  
 ### N-gram Language Model : unigram, bigram, trigram
 
-### Word prediction algorithm : 첫 2~3 어절이 주어졌을 때 다음 어절로 올 가능성이 가장 높은 어절을 생성
- 
-### Smoothing : sparseness problem 문제를 해결을 위해서 / 확률이 0이 되지 않도록 하는 방법
- - Back-off smoothing : trigram 확률 값을 구한 값이 0인 경우 bigram으로 높은 확률을 구해서 0을 만들지 않는다. 또한 bigram도 0이 나온다면 unigram 확률로 낮춰서 구함 (이때 알파를 적절한 값으로 곱한다.)
- 
- - Linear Interpolation : 기본 베이스 값도 반영 ex) trigram 확률 구할때 unigram 확률, bigram 확률 각각 비율을 정해서 반영
- => 확률 0인 값이 나오지 않게 하기 위해서
- 
- - Maximum Entropy : 전체 분포 h 중에서 wi가 차지할 비중이 얼마나 될까를 구하는 방식
-
 ```
 • Estimate probability of each word given prior context. – P(phone | Please turn off your cell)
 • “n-gram” = sequence of n words – Unigram: P(phone)
@@ -29,3 +19,16 @@
 P(your) * P(cell | your) * P(phone | your cell) * P(is | your cell phone)
 * P(good | your cell phone is)
 ```
+```
+P(B|A) = P(A교집합B) / P(A)
+P(car | your) = P( your car ) / P(your)
+```
+### Word prediction algorithm : 첫 2~3 어절이 주어졌을 때 다음 어절로 올 가능성이 가장 높은 어절을 생성
+ 
+### Smoothing : sparseness problem 문제를 해결을 위해서 / 확률이 0이 되지 않도록 하는 방법
+ - Back-off smoothing : trigram 확률 값을 구한 값이 0인 경우 bigram으로 높은 확률을 구해서 0을 만들지 않는다. 또한 bigram도 0이 나온다면 unigram 확률로 낮춰서 구함 (이때 알파를 적절한 값으로 곱한다.)
+ 
+ - Linear Interpolation : 기본 베이스 값도 반영 ex) trigram 확률 구할때 unigram 확률, bigram 확률 각각 비율을 정해서 반영
+ => 확률 0인 값이 나오지 않게 하기 위해서
+ 
+ - Maximum Entropy : 전체 분포 h 중에서 wi가 차지할 비중이 얼마나 될까를 구하는 방식
--- src/main/java/com/company/anlysis/TextAnalysis.java
+++ src/main/java/com/company/anlysis/TextAnalysis.java
@@ -9,17 +9,21 @@ import java.util.Map;
 import org.apache.commons.io.FileUtils;
 
 public class TextAnalysis {
-	public static Map<String, String> map;
+	public static Map<String, String> unigramMap = new HashMap<String, String>();
+	public static Map<String, String> bigramMap = new HashMap<String, String>();
+	public static Map<String, String> trigramMap = new HashMap<String, String>();
 	
-	public static void setMap(String filename) throws IOException {
+	/**
+	 * function to access easily value about count and string
+	 */
+	public static void setMap(String filename, Map<String, String> map) throws IOException {
 		String dir = filename;
     	File dirFile = new File(dir);
-		map = new HashMap<String, String>();
 		
 		long start = System.currentTimeMillis();
 		List<String> readList = FileUtils.readLines(dirFile, "UTF-8");
 		long end = System.currentTimeMillis();
-		System.out.println("Excution time : "+ (end-start)/1000.0+ "second");
+		System.out.println(filename+" Time to read: "+ (end-start)/1000.0+ " second");
 		
 		long startFor = System.currentTimeMillis();
 		String[] input;
@@ -28,17 +32,22 @@ public class TextAnalysis {
 			// remove front-end space
 			String result = readList.get(i).replaceAll("(^\\p{Z}+|\\p{Z}+$)", "");
 			
-			input = result.split("\t");
+			input = result.split("\t"); // split count number and string value
 			
 			if(input.length == 2) map.put(input[1], input[0]);
 			
 		}
 		long endFor = System.currentTimeMillis();
-		System.out.println("Excution time : "+ (endFor-startFor)/1000.0+ "second");
-		
+
+		System.out.println("Time to make hashMap: "+ (endFor-startFor)/1000.0+ " second");
 	}
 	public static void main(String[] args) throws IOException
 	{
-		setMap("trigramCount.txt");
+		setMap("uniCount.txt", unigramMap);
+		
+		String text = "자기 유도 전류는 역선으로 표현된다";
+		String str = "전류";
+		System.out.println(unigramMap.get(str));
+		
 	}
 }
